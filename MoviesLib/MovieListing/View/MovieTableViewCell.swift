c7d21858bc9b0c2089d5c3c7fb640233c9ea0b55
Kauã Estriga <kaua222@gmail.com>
2020-09-24 22:24:56 -0300

    moviewcontrollercell

--- MoviesLib/MovieListing/View/MovieTableViewCell.swift
+++ MoviesLib/MovieListing/View/MovieTableViewCell.swift
@@ -9,16 +9,23 @@
 import UIKit
 
 class MovieTableViewCell: UITableViewCell {
-    
-    //MARK: - IBOutlets
+    // MARK: - IBOutlets
     @IBOutlet weak var imageViewPoster: UIImageView!
     @IBOutlet weak var labelTitle: UILabel!
-    @IBOutlet weak var labelSummary: UILabel!
     @IBOutlet weak var labelRating: UILabel!
+    @IBOutlet weak var labelSummary: UILabel!
     
-    //MARK: - Methods
+    // MARK: - Methods
     func configure(with movie: Movie) {
-        if let imageViewPoster.image = UIImage(named: movie.image)
+        if let image = movie.image {
+            imageViewPoster.image = UIImage(named: image)
+        } else {
+            imageViewPoster.image = nil
+        }
+        //imageViewPoster.image = UIImage(named: movie.image ?? "placeholder")
+        labelTitle.text = movie.title
+        labelRating.text = movie.ratingFormatted
+        labelSummary.text = movie.summary
     }
 
 }
